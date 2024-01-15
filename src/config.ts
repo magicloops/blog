8f833a67ab5cd055ef34522f12139e4b75917a12
Adam Adastra <jumploops@gmail.com>
2024-01-14 23:52:55 -0800

    init commit

---
+++ src/config.ts
@@ -0,0 +1,146 @@
+import type { Site, SocialObjects } from "./types";
+
+export const SITE: Site = {
+  website: "https://magicloops.dev/", // replace this with your deployed domain
+  author: "Adam and Mihai",
+  desc: "Program Computers with Natural Languge",
+  title: "Magic Loops",
+  ogImage: "magic-loops-logo.png",
+  lightAndDarkMode: true,
+  postPerPage: 3,
+};
+
+export const LOCALE = {
+  lang: "en", // html lang code. Set this empty and default will be "en"
+  langTag: ["en-EN"], // BCP 47 Language Tags. Set this empty [] to use the environment default
+} as const;
+
+export const LOGO_IMAGE = {
+  enable: false,
+  svg: true,
+  width: 216,
+  height: 46,
+};
+
+export const SOCIALS: SocialObjects = [
+  {
+    name: "Github",
+    href: "https://github.com/magicloops",
+    linkTitle: ` ${SITE.title} on Github`,
+    active: true,
+  },
+  {
+    name: "Facebook",
+    href: "https://www.facebook.com/buildmagicloops",
+    linkTitle: `${SITE.title} on Facebook`,
+    active: true,
+  },
+  {
+    name: "Instagram",
+    href: "https://github.com/satnaing/astro-paper",
+    linkTitle: `${SITE.title} on Instagram`,
+    active: false,
+  },
+  {
+    name: "LinkedIn",
+    href: "https://www.linkedin.com/company/magicloops/",
+    linkTitle: `${SITE.title} on LinkedIn`,
+    active: true,
+  },
+  {
+    name: "Mail",
+    href: "mailto:humans@magicloops.dev",
+    linkTitle: `Send an email to ${SITE.title}`,
+    active: true,
+  },
+  {
+    name: "Twitter",
+    href: "https://twitter.com/magicloopsdev",
+    linkTitle: `${SITE.title} on Twitter`,
+    active: true,
+  },
+  {
+    name: "Twitch",
+    href: "https://github.com/satnaing/astro-paper",
+    linkTitle: `${SITE.title} on Twitch`,
+    active: false,
+  },
+  {
+    name: "YouTube",
+    href: "https://www.youtube.com/@LoopRuns",
+    linkTitle: `${SITE.title} on YouTube`,
+    active: true,
+  },
+  {
+    name: "WhatsApp",
+    href: "https://github.com/satnaing/astro-paper",
+    linkTitle: `${SITE.title} on WhatsApp`,
+    active: false,
+  },
+  {
+    name: "Snapchat",
+    href: "https://github.com/satnaing/astro-paper",
+    linkTitle: `${SITE.title} on Snapchat`,
+    active: false,
+  },
+  {
+    name: "Pinterest",
+    href: "https://github.com/satnaing/astro-paper",
+    linkTitle: `${SITE.title} on Pinterest`,
+    active: false,
+  },
+  {
+    name: "TikTok",
+    href: "https://github.com/satnaing/astro-paper",
+    linkTitle: `${SITE.title} on TikTok`,
+    active: false,
+  },
+  {
+    name: "CodePen",
+    href: "https://github.com/satnaing/astro-paper",
+    linkTitle: `${SITE.title} on CodePen`,
+    active: false,
+  },
+  {
+    name: "Discord",
+    href: "https://discord.gg/78DJrRtwfW",
+    linkTitle: `${SITE.title} on Discord`,
+    active: true,
+  },
+  {
+    name: "GitLab",
+    href: "https://github.com/satnaing/astro-paper",
+    linkTitle: `${SITE.title} on GitLab`,
+    active: false,
+  },
+  {
+    name: "Reddit",
+    href: "https://github.com/satnaing/astro-paper",
+    linkTitle: `${SITE.title} on Reddit`,
+    active: false,
+  },
+  {
+    name: "Skype",
+    href: "https://github.com/satnaing/astro-paper",
+    linkTitle: `${SITE.title} on Skype`,
+    active: false,
+  },
+  {
+    name: "Steam",
+    href: "https://github.com/satnaing/astro-paper",
+    linkTitle: `${SITE.title} on Steam`,
+    active: false,
+  },
+  {
+    name: "Telegram",
+    href: "https://github.com/satnaing/astro-paper",
+    linkTitle: `${SITE.title} on Telegram`,
+    active: false,
+  },
+  {
+    name: "Mastodon",
+    href: "https://github.com/satnaing/astro-paper",
+    linkTitle: `${SITE.title} on Mastodon`,
+    active: false,
+  },
+];
---
+++ src/pages/about.md
@@ -0,0 +1,37 @@
+---
+layout: ../layouts/AboutLayout.astro
+title: "About"
+---
+
+Magic Loops are the fastest way to combine LLMs and code to create simple automations. 
+
+Our goal is to give everyone the superpower of programming, without having to learn how to code.
+
+We think no-code and low-code tools are too limiting, while full-code IDEs are often too complex, even for CS students.
+
+Magic Loops bridge the gap between both extremes, married together with the use of state-of-the-art generative AI.        
+
+<div>
+  <img src="/assets/dev.svg" class="sm:w-1/2 mx-auto" alt="coding dev illustration">
+</div>
+
+## Tech Stack
+
+Magic Loops run on AWS, and utilize a variety of 3rd party APIs to perform tasks on your behalf. We use GPT-4 and GPT-3.5 from OpenAI for our "LLM Blocks", Twilio for our "Send SMS Blocks", and a number of included API providers for fetching data for weather, finance, news data, and more.
+
+## Features
+
+Magic Loops are just code, and almost anything you can do with code, you can do with Magic Loops. 
+
+- ChatGPT automations (e.g. daily meditation prompt)
+- text and email notifications (e.g. TSLA stock up more than 5%)
+- custom API endpoints (GET, POST, etc.)
+- web scraping (e.g. send me the top 5 stories on Hacker News)
+- system integrations (e.g. connect Cloudflare to Mixpanel)
+- background tasks (e.g. when user signs up, update CRM, send welcome email, invite to Slack)
+- GPT Actions (use Magic Loops to empower GPTs)
+- Discord Bots
+- ...and more! (surprise us!)
+
+If you like this theme, you can star/contribute to the [repo](https://github.com/satnaing/astro-paper).  
+Or you can even give any feedback via my [email](mailto:contact@satnaing.dev).
